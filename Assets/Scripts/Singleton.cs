4048f297647e1382b533958b700c2d08e375ca94
Donray Williams <w.donray@yahoo.com>
2018-12-18 23:19:29 -0600

    Update to Singleton class

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -5,9 +5,11 @@ namespace GenericManagers
     public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
         //  Check to see if we're about to be destroyed
-        private static bool _shuttingDown = false;
+        private static bool _applicationIsQuitting;
 
         private static T _instance;
+
+        private static readonly object Lock = new object();
 
         /// <summary>
         ///     Access singleton instance through this property
@@ -16,41 +18,55 @@ namespace GenericManagers
         {
             get
             {
-                if (_shuttingDown)
+                if (_applicationIsQuitting)
                 {
                     Debug.Log("<color=yellow> [Singleton] Instance " + typeof(T) +
                               " already destroyed returning null </color>");
                     return null;
                 }
 
-                if (_instance == null)
+                lock (Lock)
                 {
-                    // Search for exisiting instance
-                    _instance = (T) FindObjectOfType(typeof(T));
-
-                    //  Create a new instance if one does not exist
                     if (_instance == null)
                     {
-                        var singletonObject = new GameObject();
-                        _instance = singletonObject.AddComponent<T>();
-                        singletonObject.name = "----- " + typeof(T) + " -----";
-                        //  Make instance persistent
-                        DontDestroyOnLoad(singletonObject);
+                        // Search for exisiting instance
+                        _instance = (T)FindObjectOfType(typeof(T));
+
+                        if (FindObjectsOfType(typeof(T)).Length > 1)
+                        {
+                            Debug.Log("<color=yellow> [Singleton] Something went really wrong " + typeof(T) +
+                                           " - there should never be more than 1 singleton!" +
+                                           " Reopenning the scene might fix it. </color>");
+                            return _instance;
+                        }
+
+                        //  Create a new instance if one does not exist
+                        if (_instance == null)
+                        {
+                            var singletonObject = new GameObject();
+                            _instance = singletonObject.AddComponent<T>();
+                            singletonObject.name = "----- " + typeof(T) + " -----";
+                            //  Make instance persistent
+                            DontDestroyOnLoad(singletonObject);
+
+                            Debug.Log("<color=yellow> [Singleton] An instance of " + typeof(T) +
+                                      " is needed in the scene, so '" + singletonObject +
+                                      "' was created with DontDestroyOnLoad.  </color>");
+                        }
+                        else
+                        {
+                            Debug.Log("<color=yellow> [Singleton] Using instance already created: " +
+                                      _instance.gameObject.name + " </color>");
+                        }
                     }
                 }
-
                 return _instance;
             }
         }
 
-        private void OnApplicationQuit()
-        {
-            _shuttingDown = true;
-        }
-
         private void OnDestroy()
         {
-            _shuttingDown = true;
+            _applicationIsQuitting = true;
         }
     }
 }
