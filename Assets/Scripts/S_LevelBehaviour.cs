328a0fbd6b9b828e080fc1e29eba5c39f7f15925
regireed89 <regireed89@gmail.com>
2018-12-28 16:25:15 -0600

    cut scene working
    did cut scene.
    fade between scenes needs more work

--- Assets/Scripts/S_LevelBehaviour.cs
+++ Assets/Scripts/S_LevelBehaviour.cs
@@ -3,24 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
+using UnityEngine.UI;
 
 namespace GenericManagers
 {
     public class S_LevelBehaviour : Singleton<S_LevelBehaviour>
     {
-
-        public void Start()
+        VideoPlayer videoPlayer;
+        public void Awake()
         {
             // Will attach a VideoPlayer to the main camera.
             GameObject camera = GameObject.Find("Main Camera");
 
             // VideoPlayer automatically targets the camera backplane when it is added
             // to a camera object, no need to change videoPlayer.targetCamera.
-            var videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
+            videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
+
 
             // Play on awake defaults to true. Set it to false to avoid the url set
             // below to auto-start playback since we're in Start().
-            videoPlayer.playOnAwake = false;
+            //videoPlayer.playOnAwake = false;
+
+            // By default, VideoPlayers added to a camera will use the far plane.
+            // Let's target the near plane instead.
+            videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
+            videoPlayer.SetDirectAudioVolume(0, .2f);
         }
 
         /// <summary>
@@ -52,22 +59,27 @@ namespace GenericManagers
         /// loads scene in the background of current scene
         /// </summary>
         /// <param name="levelIndex"></param>
-        public void LoadLevelAsync(int levelIndex)
+        public AsyncOperation LoadLevelAsync(int levelIndex)
         {
-            SceneManager.LoadSceneAsync(levelIndex);
+            return SceneManager.LoadSceneAsync(levelIndex);
+        }
+        public AsyncOperation LoadLevelAsync(string levelName)
+        {
+            return SceneManager.LoadSceneAsync(levelName);
         }
 
         /// <summary>
-        /// play video/cutscene
+        /// play video/cutscene. Takes in the file path to video
         /// </summary>
         /// <param name="video"></param>
-        public void PlayCutScene(VideoPlayer video)
+        public void PlayCutScene(string videoFilePath)
         {
+            videoPlayer.url = videoFilePath;
             //pepares the video for playback
-            video.Prepare();
+            videoPlayer.Prepare();
             //checks if video is ready for play then plays it
-            if (video.isPrepared)
-                video.Play();
+            if (videoPlayer.isPrepared)
+                videoPlayer.Play();
         }
 
         /// <summary>
@@ -78,15 +90,27 @@ namespace GenericManagers
             SceneManager.LoadScene(SceneManager.GetActiveScene().ToString());
         }
 
-        public void OpenMenu(string menuName)
+        public void FadeToScene()
         {
+            //loads the fade out animation
+            Resources.Load("C: /Users/regir/Documents/GitHub/Generic - Game - Template/Assets/Resources/FadeOut.anim");
+
+            var image = GameObject.FindObjectOfType<Image>().gameObject;
+            var targetScene = SceneManager.GetSceneByName("TestScene2");
+            var l = LoadLevelAsync(targetScene.ToString());
+            //moves image object to scene that will be loaded
+            SceneManager.MoveGameObjectToScene(image, targetScene);
+            //loads next scene in background;
+            
+            if (l.isDone)
+            {
+                l.allowSceneActivation = true;
+                Resources.Load("C: /Users/regir/Documents/GitHub/Generic - Game - Template/Assets/Resources/FadeIn.anim");
+
+            }
 
         }
-
-        public void OpenOptions()
-        {
-
-        }
+        
 
     }
 }
